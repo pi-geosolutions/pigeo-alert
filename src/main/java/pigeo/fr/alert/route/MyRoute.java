5b9a4018ae9894084dd12126642f1498abc7e0cb
Florent Gravin <florent.gravin@camptocamp.com>
2018-02-04 21:54:23 +0100

    Add Camel context

---
+++ src/main/java/pigeo/fr/alert/route/MyRoute.java
@@ -0,0 +1,32 @@
+package pigeo.fr.alert.route;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by florent on 04/02/18.
+ */
+
+@Component
+public class MyRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("file:/home/florent/dev/test_camel")
+                .convertBodyTo(String.class)
+                .aggregate(constant(true)).completionFromBatchConsumer().groupExchanges()
+                .bean(HandlerBean.class, "process")
+                .split(exchangeProperty("ZONE_IDS"))
+                    .log(LoggingLevel.INFO, "pigeo.fr.alert.route", "${body}")
+                    .to("direct:sendMail")
+                    .end()
+        ;
+
+        from("direct:sendMail")
+                .setHeader("subject", constant("HELLO FLOW"))
+                .setHeader("to", constant("florent.gravin@gmail.com"));
+//                .recipientList(simple("smtps://smtp.gmail.com:465?username=${property.smtpuser}&password=${property.smtppassword}")); This works
+    }
+}
