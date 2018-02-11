bd5ca54c4b05cee25d721bbadcede5d74ec234a1
Florent Gravin <florent.gravin@camptocamp.com>
2018-02-11 16:41:35 +0100

    Remove useless java imports

--- src/main/java/pigeo/fr/alert/Application.java
+++ src/main/java/pigeo/fr/alert/Application.java
@@ -1,20 +1,12 @@
 package pigeo.fr.alert;
 
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.camel.component.jackson.JacksonDataFormat;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/pigeo/fr/alert/ExposeEntityIdRestConfiguration.java
+++ src/main/java/pigeo/fr/alert/ExposeEntityIdRestConfiguration.java
@@ -1,16 +1,9 @@
 package pigeo.fr.alert;
 
-import com.bedatadriven.jackson.datatype.jts.JtsModule;
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import pigeo.fr.alert.domain.User;
-
-import java.text.SimpleDateFormat;
 
 /**
  * Created by florent on 05/02/18.
