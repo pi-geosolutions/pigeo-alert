00c5c7cacadc3db324077fa16a274710cc35b30c
Florent Gravin <florent.gravin@camptocamp.com>
2018-03-10 11:49:21 +0100

    Remove dashboard ui module

--- src/main/webapp/alert-ui/src/app/app.module.ts
+++ src/main/webapp/alert-ui/src/app/app.module.ts
@@ -19,7 +19,6 @@ import {ZoneService} from "./zone/zone.service";
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { ZoneSearchComponent } from './zone/zone-search/zone-search.component';
 import { MapDirective } from './ngeo/map/map.directive';
 import { GeometryToolComponent } from './ngeo/geom/geometry-tool/geometry-tool.component';
@@ -84,7 +83,6 @@ import { HomeComponent } from './home/home.component';
     UserComponent,
     UserDetailComponent,
     MessagesComponent,
-    DashboardComponent,
     ZoneSearchComponent,
     UserSearchComponent,
     MapDirective,
